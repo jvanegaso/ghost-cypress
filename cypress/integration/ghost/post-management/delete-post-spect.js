eab160734c94bb97e77677dc4e0c587926368cb6
LauraSilvaAyala <lm.silvaa1@uniandes.edu.co>
2021-05-10 22:14:40 -0500

    commit the elements

--- cypress/integration/ghost/post-management/delete-post-spect.js
+++ cypress/integration/ghost/post-management/delete-post-spect.js
@@ -10,26 +10,26 @@ let postsPage = null;
 let postPage = null;
 let layoutPage = null;
 
-describe('Eliminación de post', () => {
+//describe('Eliminación de post', () => {
 
-    before('Setup', () => {
-       loginPage = new LoginPage();
-       postsPage = new PostsPage();
-       postPage = new PostPage();
-       layoutPage = new LayoutPage();
-    });
+  //  before('Setup', () => {
+   //    loginPage = new LoginPage();
+   //    postsPage = new PostsPage();
+   //    postPage = new PostPage();
+   //    layoutPage = new LayoutPage();
+   // });
   
-    it('Deberia Eliminar el post de manera correcta .', () => {
+ //   it('Deberia Eliminar el post de manera correcta .', () => {
       // Given a title and description 
-       const titlePostToDelete  = 'Welcome to Test Ghost';
+   //    const titlePostToDelete  = 'Welcome to Test Ghost';
        // cy.login(null,null,true);
       // cy.createPost(titlePostToDelete, '');
        // When a user try to create a post 
       // cy.deletePost(titlePostToDelete);
       // Then the application display a message that the post was published.
-        cy.url().should('include','#/post');
+     //   cy.url().should('include','#/post');
 
-        cy.logout();
-    });
+       // cy.logout();
+    //});
 
-  });
+  //});
