5486dba4aa387fbbe3c975ab90a49b39c0f6809a
LauraSilvaAyala <lm.silvaa1@uniandes.edu.co>
2021-05-10 22:13:51 -0500

    Commit the elements

--- cypress/integration/ghost/post-management/edit-post-spect.js
+++ cypress/integration/ghost/post-management/edit-post-spect.js
@@ -10,32 +10,32 @@ let postsPage = null;
 let postPage = null;
 let layoutPage = null;
 
-describe('Edición de post', () => {
+//describe('Edición de post', () => {
 
-    before('Setup', () => {
-       loginPage = new LoginPage();
-       postsPage = new PostsPage();
-       postPage = new PostPage();
-       layoutPage = new LayoutPage();
+  //  before('Setup', () => {
+   //    loginPage = new LoginPage();
+   //    postsPage = new PostsPage();
+   //    postPage = new PostPage();
+   //    layoutPage = new LayoutPage();
  
-    });
+  //  });
   
-    it('Deberia publicar el post de manera correcta .', () => {
+    //it('Deberia publicar el post de manera correcta .', () => {
       // Given a title and description 
-       const titlePostToEdit  = 'Welcome to Test Ghost';
-       const textToAddPostToEdit = 'Welcome to Horrible Ghost';
+      // const titlePostToEdit  = 'Welcome to Test Ghost';
+      // const textToAddPostToEdit = 'Welcome to Horrible Ghost';
   
-       cy.createPost(titlePostToEdit,'');
-       cy.wait(2000);
+      // cy.createPost(titlePostToEdit,'');
+      // cy.wait(2000);
        // When a user try to create a post 
        //cy.editPost(titlePostToEdit,textToAddPostToEdit)
   
       // Then the application display a message that the post was published.
-      layoutPage.getNotificationWrapper()
-        .contains('Published!')
-         .should('be.visible');
+     // layoutPage.getNotificationWrapper()
+      //  .contains('Published!')
+       //  .should('be.visible');
        //  cy.logout();
        //  cy.wait(2000);
-    });
+    //});
 
-  });
+ // }); the 
